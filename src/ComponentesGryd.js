db2a1323b4d426686df1154bf1dfc0a5b44c33f5
Cassandra1245 <guillermogarciasuarez@gmail.com>
2025-01-08 21:38:11 +0100

    third commit

--- src/ComponentesGryd.js
+++ src/ComponentesGryd.js
@@ -4,14 +4,72 @@ function MyGrid() {
     return (
         <Container>
             <Row>
-                <Col xs={12} md={6} xxl={4}>
-                    <div>Mujeres:</div>
+                <Col xs={6} md={6} xxl={6}>
+                    <p>Primera Columna:</p>
+                    <div
+                        className="m-3"
+                        style={{
+                            backgroundImage: "url('./FondoAzul.jpeg')",
+                            width: '300px',
+                            height: '200px',
+                        }}
+                    />
+                    <p className="m-3">AAAAAAA OOOOOOO AAAAAAA OOOOOOO AAAAAAA OOOOOOO</p>
                 </Col>
-                <Col xs={12} md={6} xxl={4}>
-                    <div>Hombres:</div>
-                </Col>
-                <Col xs={12} md={12} xxl={4}>
-                    <div>Niños:</div>
+
+                <Col xs={3} md={3} xxl={3}>
+                    <div
+                        className="m-3 d-flex align-items-center"
+                        style={{
+                            backgroundImage: "url('./FondoAzul.jpeg')",
+                            width: '100%', // Ancho completo para todo el contenedor
+                            height: '100px', // Alto del contenedor
+                        }}
+                    >
+                        <div
+                            style={{
+                                backgroundImage: "url('./FondoAzul.jpeg')",
+                                backgroundSize: 'cover', // Ajusta la imagen para que ocupe todo el espacio del div
+                                width: '50px', // Ancho fijo para la imagen
+                                height: '50px', // Alto fijo para la imagen
+                                marginRight: '10px', // Espacio entre la imagen y el texto
+                            }}
+                        />
+                        <p>AYIYIYIYIYIYIYIYI</p>
+                    </div>
+
+                    <div
+                        className="m-3 d-flex"
+                        style={{
+                            backgroundImage: "url('./FondoAzul.jpeg')",
+                            width: '150px',
+                            height: '100px',
+                        }}
+                    >
+                        <p>AYIYIYIYIYIYIYIYI</p>
+                    </div>
+
+                    <div
+                        className="m-3 d-flex"
+                        style={{
+                            backgroundImage: "url('./FondoAzul.jpeg')",
+                            width: '150px',
+                            height: '100px',
+                        }}
+                    >
+                        <p>AYIYIYIYIYIYIYIYI</p>
+                    </div>
+
+                    <div
+                        className="m-3 d-flex"
+                        style={{
+                            backgroundImage: "url('./FondoAzul.jpeg')",
+                            width: '150px',
+                            height: '100px',
+                        }}
+                    >
+                        <p>AYIYIYIYIYIYIYIYI</p>
+                    </div>
                 </Col>
             </Row>
         </Container>
