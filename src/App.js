2324e0e847dbc2cada9249427b5218e2ed429c88
Cassandra1245 <guillermogarciasuarez@gmail.com>
2025-01-08 20:50:27 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -8,29 +8,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
 
 
-        <div class="m-3">Este elemento tiene un margen de 3rem en todos los lados.</div>
-        <div class="p-2">Este elemento tiene un relleno de 2rem en todos los lados.</div>
-        <div class="mt-4">Este elemento tiene un margen superior de 4rem.</div>
-        <div class="mx-auto">Este elemento está centrado horizontalmente.</div>
-
-        <div class="p-3 md:p-5">
-          Este elemento tiene un relleno de 3rem en todas las pantallas.
-          En pantallas medianas (md) o más grandes, el relleno aumenta a 5rem.
-        </div>
-        
         <MyGrid />
 
       </header>
--- src/ComponentesGryd.js
+++ src/ComponentesGryd.js
@@ -1,18 +1,22 @@
 import { Container, Row, Col } from 'react-bootstrap';
 
 function MyGrid() {
-  return (
-    <Container>
-      <Row>
-        <Col>
-          <div>Columna 1</div>
-        </Col>
-        <Col>
-          <div>Columna 2</div>
-        </Col>
-      </Row>
-    </Container>
-  );
+    return (
+        <Container>
+            <Row>
+                <Col xs={12} md={6} xxl={4}>
+                    <div>Mujeres:</div>
+                </Col>
+                <Col xs={12} md={6} xxl={4}>
+                    <div>Hombres:</div>
+                </Col>
+                <Col xs={12} md={12} xxl={4}>
+                    <div>Niños:</div>
+                </Col>
+            </Row>
+        </Container>
+    );
+
 }
 
 export default MyGrid;
