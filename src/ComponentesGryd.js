4f6e912cdf0423d7cff24afe662d1e0f56afa9f3
Cassandra1245 <guillermogarciasuarez@gmail.com>
2025-01-10 13:50:26 +0100

    quedaRetocar

--- src/ComponentesGryd.js
+++ src/ComponentesGryd.js
@@ -4,10 +4,10 @@ function MyGrid() {
     return (
         <Container>
             <Row>
-                <Col xs={6} md={6} xxl={6}>
-                    <p>Primera Columna:</p>
+                <Col className="m-3 align-items-center" xs={12} md={6} xxl={6}>
+                    <p className="m-3">Primera Columna:</p>
                     <div
-                        className="m-3"
+                    className="m-3"
                         style={{
                             backgroundImage: "url('./FondoAzul.jpeg')",
                             width: '300px',
@@ -17,57 +17,55 @@ function MyGrid() {
                     <p className="m-3">AAAAAAA OOOOOOO AAAAAAA OOOOOOO AAAAAAA OOOOOOO</p>
                 </Col>
 
-                <Col xs={3} md={3} xxl={3}>
-                    <div
-                        className="m-3 d-flex align-items-center"
-                        style={{
-                            backgroundImage: "url('./FondoAzul.jpeg')",
-                            width: '100%', // Ancho completo para todo el contenedor
-                            height: '100px', // Alto del contenedor
-                        }}
-                    >
+                <Col className="m-3" xs={12} md={3} xxl={3}>
+                    <div className="d-flex align-items-center" style={{ minWidth: '150px' }}>
                         <div
+                            className="m-3"
                             style={{
                                 backgroundImage: "url('./FondoAzul.jpeg')",
-                                backgroundSize: 'cover', // Ajusta la imagen para que ocupe todo el espacio del div
-                                width: '50px', // Ancho fijo para la imagen
-                                height: '50px', // Alto fijo para la imagen
-                                marginRight: '10px', // Espacio entre la imagen y el texto
+                                width: '100%',
+                                height: '100px',
+                                minWidth: '150px',
+                            }}
+                        />
+                        <p>AYIYIYIYIYIYIYIYI</p>
+                    </div>
+                    <div className="d-flex align-items-center" style={{ minWidth: '150px' }}>
+                        <div
+                            className="m-3"
+                            style={{
+                                backgroundImage: "url('./FondoAzul.jpeg')",
+                                width: '100%',
+                                height: '100px',
+                                minWidth: '150px', 
                             }}
                         />
                         <p>AYIYIYIYIYIYIYIYI</p>
                     </div>
 
-                    <div
-                        className="m-3 d-flex"
-                        style={{
-                            backgroundImage: "url('./FondoAzul.jpeg')",
-                            width: '150px',
-                            height: '100px',
-                        }}
-                    >
+                    <div className="d-flex align-items-center" style={{ minWidth: '150px' }}>
+                        <div
+                            className="m-3"
+                            style={{
+                                backgroundImage: "url('./FondoAzul.jpeg')",
+                                width: '100%',
+                                height: '100px',
+                                minWidth: '150px',
+                            }}
+                        />
                         <p>AYIYIYIYIYIYIYIYI</p>
                     </div>
 
-                    <div
-                        className="m-3 d-flex"
-                        style={{
-                            backgroundImage: "url('./FondoAzul.jpeg')",
-                            width: '150px',
-                            height: '100px',
-                        }}
-                    >
-                        <p>AYIYIYIYIYIYIYIYI</p>
-                    </div>
-
-                    <div
-                        className="m-3 d-flex"
-                        style={{
-                            backgroundImage: "url('./FondoAzul.jpeg')",
-                            width: '150px',
-                            height: '100px',
-                        }}
-                    >
+                    <div className="d-flex align-items-center" style={{ minWidth: '150px' }}>
+                        <div
+                            className="m-3"
+                            style={{
+                                backgroundImage: "url('./FondoAzul.jpeg')",
+                                width: '100%',
+                                height: '100px',
+                                minWidth: '150px', 
+                            }}
+                        />
                         <p>AYIYIYIYIYIYIYIYI</p>
                     </div>
                 </Col>
