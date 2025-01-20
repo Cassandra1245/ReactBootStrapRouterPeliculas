9f433f6503bff1acb927559a7b5c206a90873c3a
Cassandra1245 <guillermogarciasuarez@gmail.com>
2025-01-20 21:33:38 +0100

    BarraNavegacionEnProceso

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { Container, Col, Row, Card, CardBody, ListGroup, CardFooter, Button } from "react-bootstrap";
+import { Navbar, Nav, NavDropdown, Container, Col, Row, Card, CardBody, ListGroup, CardFooter, Button } from "react-bootstrap";
 import './App.css';
 
 function App() {
 
   const [peliculas, setPeliculas] = useState([]);
   const [Boton, setBoton] = useState({});
+  const [generos, setGeneros] = useState({})
 
   useEffect(() => {
     const fetchPeliculas = async () => {
@@ -23,35 +24,71 @@ function App() {
     }));
   };
 
+  const seleccionar = (titulo) => {
+    setBoton((prevState) => ({
+      ...prevState,
+      [titulo]: !prevState[titulo],
+    }));
+  };
+
   return (
     <div className="App">
-        <Row>
-          {peliculas.map((dato) => (
-            <Col xs={12} md={3} xl={3}>
-              <Card key={dato.titulo} className="m-3">
-                <Card.Img variant="top" src={dato.foto} style={{ width: "100%" }} />
-                <Card.Header>
-                  <Card.Title>{dato.titulo}</Card.Title>
-                </Card.Header>
-                <CardBody>
-                  <ListGroup className="list-group-flush">
-                    <ListGroup.Item> Actores: {dato.actoresPrincipales}</ListGroup.Item>
-                    <ListGroup.Item>Director: {dato.director}</ListGroup.Item>
-                    <ListGroup.Item>Actores Principales: {dato.actoresPrincipales}</ListGroup.Item>
-                  </ListGroup>
-                </CardBody>
-                <Button variant="primary" onClick={() => mostrar(dato.titulo)}>
-                  {Boton[dato.titulo] ? "Menos" : "Mas"}
-                </Button>
-                {Boton[dato.titulo] && (
-                  <CardFooter>
-                    <Card.Text> {dato.sinopsis}</Card.Text>
-                  </CardFooter>
-                )}
-              </Card>
-            </Col>
-          ))}
-        </Row>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="./FondoAzul.jpeg">Peliculas</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <NavDropdown title="Directores" id="basic-nav-dropdown">
+                {peliculas.map((dato) => (
+                  <div key={dato.titulo}>
+                    <NavDropdown.Item href="#action/3.1">{dato.director}</NavDropdown.Item>
+                  </div>
+                ))}
+              </NavDropdown>
+              <NavDropdown title="Generos" id="basic-nav-dropdown">
+                {peliculas.map((dato) => (
+                  <div key={dato.titulo}>
+                    <NavDropdown.Item href="#action/3.1">{dato.categoria}</NavDropdown.Item>
+                  </div>
+                ))}
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+
+      <Row>
+        {peliculas.map((dato) => (
+          <Col xs={12} md={3} xl={3}>
+            <Card key={dato.titulo} className="m-3">
+              <Card.Img variant="top" src={dato.foto} style={{ width: "100%" }} />
+              <Card.Header>
+                <Card.Title>{dato.titulo}</Card.Title>
+              </Card.Header>
+              <CardBody>
+                <ListGroup className="list-group-flush">
+                  <ListGroup.Item> Actores: {dato.actoresPrincipales}</ListGroup.Item>
+                  <ListGroup.Item>Director: {dato.director}</ListGroup.Item>
+                  <ListGroup.Item>Actores Principales: {dato.actoresPrincipales}</ListGroup.Item>
+                </ListGroup>
+              </CardBody>
+              <Button variant="primary" onClick={() => mostrar(dato.titulo)}>
+                {Boton[dato.titulo] ? "Menos" : "Mas"}
+              </Button>
+              <Button variant="secondary" onClick={() => seleccionar(dato.titulo)}>
+                Seleccionar
+              </Button>
+              {Boton[dato.titulo] && (
+                <CardFooter>
+                  <Card.Text> {dato.sinopsis}</Card.Text>
+                </CardFooter>
+              )}
+            </Card>
+          </Col>
+        ))}
+      </Row>
     </div>
   );
 }
